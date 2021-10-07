d377fbef916b0b080387c16b315cf80b4092c30c
Bastiaan Olij <mux213@gmail.com>
2021-10-07 17:16:59 +1100

    Add new head tracking code

---
+++ demo/Hud.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+@export var camera : NodePath
+
+func _ready():
+	var material : StandardMaterial3D = $Display.get_surface_override_material(0)
+	if material:
+		material.albedo_texture = $FPSViewport.get_texture()
+
+func _process(delta):
+	if camera:
+		var camera_node : XRCamera3D = get_node(camera)
+		
+		if camera_node:
+			var t = camera_node.transform
+			var forward : Vector3 = -t.basis.z
+			forward.y = 0.0
+			forward.normalized()
+
+			position = t.origin
+
+			look_at(t.origin + forward)
+
+	var fps = Performance.get_monitor(Performance.TIME_FPS)
+	$FPSViewport/FPS.text = "FPS: " + str(fps)
--- demo/Main.gd
+++ demo/Main.gd
@@ -1,10 +1,12 @@
 extends Node3D
 
+var xr_interface : XRInterfaceReference
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Interfaces: " + str(XRServer.get_interfaces()))
 	
-	var xr_interface = XRServer.find_interface("XR Reference")
+	xr_interface = XRServer.find_interface("XR Reference")
 	if xr_interface:
 		print("Found " + str(xr_interface.get_name()))
 		print("Capabilities " + str(xr_interface.get_capabilities()))
@@ -19,3 +21,19 @@ func _ready():
 	else:
 		print("Couldn't find interface")
 
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			if xr_interface:
+				xr_interface.use_mouse_for_headtracking = false
+				xr_interface.use_wasd_for_movement = false
+			
+			# Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	elif event is InputEventMouseButton:
+		if event.pressed:
+			if xr_interface:
+				xr_interface.use_mouse_for_headtracking = true
+				xr_interface.use_wasd_for_movement = true
+			
+			# until we can receive proper input events in our interface we don't have access to the mouse when captured
+			# Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
